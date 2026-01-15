498d22a237c87d85b853a6aa6a56b8e80793b42c
Ulvi Cesur <ucesur@gmail.com>
2026-01-15 16:26:28 +0300

    Implement CORS support in translate.js
    Added CORS headers to support Flutter Web.

--- api/translate.js
+++ api/translate.js
@@ -1,6 +1,15 @@
 const axios = require("axios");
 
 module.exports = async function (req, res) {
+  // CORS ayarı (Flutter Web için)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // preflight request
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "POST only" });
   }
